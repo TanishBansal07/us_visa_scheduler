0a7ac33dd8b9539b0c3b7e42df1d3dd1d8b967de
Tanish Bansal <tbansal7@my.bcit.ca>
2025-03-13 00:01:38 -0700

    pseudo post request

--- main/example.js
+++ main/example.js
@@ -23,8 +23,40 @@ async function sign_in () {
   let visa = new us_visa(signInLink, userEmail, userPassword, userCountryCode, userConsulate);
   await visa.sign_in(page);
   await visa.go_to_appointment_page(page);
+  setTimeout(() => {}, 5000);
   let latest_date = await visa.get_available_dates(page);
+  setTimeout(() => {}, 5000);
   let time = await visa.get_available_times(page, latest_date);
-};
 
+  let authToken = await page.getAttribute('input[name="authenticity_token"]', 'value');
+// Send the POST request
+  setTimeout(() => {}, 5000);
+//   let response = await page.request.post('https://ais.usvisa-info.com/en-ca/niv/schedule/66236035/appointment', {
+//   headers: {
+//       "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+//       "accept-language": "en-US,en;q=0.9",
+//       "cache-control": "max-age=0",
+//       "content-type": "application/x-www-form-urlencoded",
+//       "sec-ch-ua": "\"Not:A-Brand\";v=\"24\", \"Chromium\";v=\"134\"",
+//       "sec-ch-ua-mobile": "?0",
+//       "sec-ch-ua-platform": "\"Windows\"",
+//       "sec-fetch-dest": "document",
+//       "sec-fetch-mode": "navigate",
+//       "sec-fetch-site": "same-origin",
+//       "sec-fetch-user": "?1",
+//       "upgrade-insecure-requests": "1",
+//       "Referer": "https://ais.usvisa-info.com/en-ca/niv/schedule/66236035/appointment",
+//       "Referrer-Policy": "strict-origin-when-cross-origin"
+//   },
+//   form: {
+//       "authenticity_token": authToken,
+//       "confirmed_limit_message": "1",
+//       "use_consulate_appointment_capacity": "true",
+//       "appointments[consulate_appointment][facility_id]": "95",
+//       "appointments[consulate_appointment][date]": latest_date,
+//       "appointments[consulate_appointment][time]": time
+//   }
+// });
+
+}
 sign_in()
